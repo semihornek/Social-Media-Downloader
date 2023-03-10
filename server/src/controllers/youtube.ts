8c3fd34ce1198c7b40a1164a5037f19af3cfd92b
Semih <semih9569@gmail.com>
2023-03-11 00:36:18 +0300

    feature: add video downloading feature for youtube

--- server/src/controllers/youtube.ts
+++ server/src/controllers/youtube.ts
@@ -1,3 +1,6 @@
+import ytdl from "ytdl-core";
+import fs from "fs";
+
 import { NextFunction, Request, Response } from "express";
 
 export const getMessage = (_req: Request, res: Response, _next: NextFunction) => {
@@ -5,5 +8,15 @@ export const getMessage = (_req: Request, res: Response, _next: NextFunction) =>
     res.status(200).send("<p>Youtube Media Downloader</p>");
   } catch (error: any) {
     res.status(404).send(`Error: ${error.message}`);
+  }
+};
+
+export const downloadVideo = (req: Request, res: Response, _next: NextFunction) => {
+  try {
+    const { url } = req.body;
+    ytdl(url).pipe(fs.createWriteStream("video.mp4"));
+    res.status(200).json({ message: "Video is downloading!", url });
+  } catch (error: any) {
+    res.status(404).json({ message: error.message });
   }
 };
--- server/src/routes/youtube.ts
+++ server/src/routes/youtube.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getMessage } from "../controllers/youtube";
+import { getMessage, downloadVideo } from "../controllers/youtube";
 
 const router = Router();
 
 // GET /youtube
 router.get("/", getMessage);
+
+// GET /youtube/downdload/
+router.get("/download", downloadVideo);
 
 export default router;
